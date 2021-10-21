19d15195d59c71b01fcee91f32e212da2df783f6
slmsshk <52129128+slmsshk@users.noreply.github.com>
2021-10-21 12:03:21 +0530

    Update main.py

--- Covid 19 telegram bot/venv/main.py
+++ Covid 19 telegram bot/venv/main.py
@@ -1,22 +1,22 @@
 import requests
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-updater = Updater(token='1862942603:AAEzW9QN8u09zBK79PdcUBk2AI2bml1TWaM', use_context=True) #Replace TOKEN with your token string
+updater = Updater(token='2073446556:AAGqO6eAHFokjarnMIkUVxvoadNRhJcm1RE', use_context=True) #Replace TOKEN with your token string
 dispatcher = updater.dispatcher
 
-# def hello(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
+def hello(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
     
-# hello_handler = CommandHandler('hello', hello)
-# dispatcher.add_handler(hello_handler)
+hello_handler = CommandHandler('hello', hello)
+dispatcher.add_handler(hello_handler)
 
 updater.start_polling()
 
-# def unknown(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
     
-# unknown_handler = MessageHandler(Filters.command, unknown)
-# dispatcher.add_handler(unknown_handler)
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
 
 def summary(update, context):
     response = requests.get('https://api.covid19api.com/summary')
